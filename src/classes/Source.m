baade2a382d92631560730be73816b266d57828e
Tim Van De Looverbosch <tim.vandelooverbosch@gmail.com>
2020-10-07 13:47:57 +0200

    Rename variable

--- src/classes/Source.m
+++ src/classes/Source.m
@@ -2,17 +2,17 @@ classdef Source
     %SOURCE
     % Properties
     % ----------
-    % source_orgin_postion:     Position of the origin (center) of the
+    % source_origin_vector:      Position of the origin (center) of the
     %                           source
     %                               1x3 vector
     
     properties
-        source_orgin_postion
+        source_origin_vector
     end
     
     methods
-        function obj = Source(source_orgin_postion)
-            obj.source_orgin_postion = source_orgin_postion;
+        function obj = Source(source_origin_vector)
+            obj.source_origin_vector = source_origin_vector;
         end
     end
 end
