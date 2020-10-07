63d1afdace3c51aec89a2aba25fda0c8e79c27e1
Tim Van De Looverbosch <tim.vandelooverbosch@gmail.com>
2020-10-07 14:28:11 +0200

    Use new classes

--- src/line_scan.m
+++ src/line_scan.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 %% Define the conveyorbelt
-conv_belt = ConveyorBelt([0, 1, 0],...      % direction
+conveyor_belt = ConveyorBelt([0, 1, 0],...      % direction
                          [0.1,0,0.9],...    % normal
                          270,...            % speed
                          -75,...            % trigger position
@@ -9,22 +9,19 @@ conv_belt = ConveyorBelt([0, 1, 0],...      % direction
                          0);                % delay
 
 %% Define a geometry of the system
-geometry.source_origin_vector   = [0, 0, 471.8];
-geometry.detector_origin_vector = [0, 0, -152.5];
-
 trans = createRotationOx(deg2rad(26));
-geometry.source_origin_vector = transformPoint3d(geometry.source_origin_vector, trans);
-geometry.detector_origin_vector = transformPoint3d(geometry.detector_origin_vector, trans);
+source = Source(transformPoint3d([0, 0, 471.8], trans));
+detector_origin_vector = transformPoint3d([0, 0, -152.5], trans);
+detector = Detector(detector_origin_vector,...
+    8 * 1.3500e-01,...
+    [1, 204],...
+    unit_vect(detector_origin_vector),...
+    250);
 
-geometry.pixel_size             = 8 * 1.3500e-01;
-geometry.detector_size_px       = [1, 204];
-geometry.detector_normal        = unit_vect(geometry.detector_origin_vector);
+
 geometry.n_scans                = 250;
-geometry.line_rate              = 250;
-
 %% Create a cad_projector instance with the geometry
-my_cad_projector = cad_projector();
-my_cad_projector = my_cad_projector.build(geometry);
+cad_projector = CADProjector(source, detector);
 
 %% Load a sample mesh
 mesh = stlread('apple.stl');
@@ -38,41 +35,38 @@ random_angle = rand(1) * 2 * pi;
 rotm = axang2rotm([random_axis, random_angle]);
 mesh.vertices = mesh.vertices * rotm;
 % Find contact point with conveyor belt
-pos = linePosition3d(mesh.vertices, [[0,0,0],conv_belt.normal]);
+pos = linePosition3d(mesh.vertices, [[0,0,0],conveyor_belt.normal]);
 translation = min(pos);
-mesh.vertices = mesh.vertices - translation * conv_belt.normal;
+mesh.vertices = mesh.vertices - translation * conveyor_belt.normal;
 % Initial position of the mesh
-start_point = -150 * conv_belt.direction;
+start_point = -150 * conveyor_belt.direction;
 moving_mesh = mesh;
 moving_mesh.vertices = moving_mesh.vertices + start_point;
 
 %% Visualize initial scene
-ray.vertices = [geometry.source_origin_vector;...
-    my_cad_projector.detector_points(1,:);...
-    my_cad_projector.detector_points(end,:)];
-ray.faces = [1, 2, 3];
-box = [geometry.source_origin_vector, 30];
+ray = cad_projector.ray;
+box = [cad_projector.source.source_origin_vector, 30];
 figure;
+hold on
 patch(moving_mesh,'FaceColor', [0.3, 0.8, 0.3], 'FaceAlpha', 0.5, 'EdgeAlpha', 0.3)
 patch(ray, 'FaceColor', 'y', 'FaceAlpha', 0.3, 'EdgeColor', 'None')
 axis equal; view(3); rotate3d on
-hold on
-scatter3(my_cad_projector.detector_points(:,1),...
-    my_cad_projector.detector_points(:,2),...
-    my_cad_projector.detector_points(:,3));
-scatter3(my_cad_projector.source_origin_vector(1),...
-    my_cad_projector.source_origin_vector(2),...
-    my_cad_projector.source_origin_vector(3), 100, 'filled', 'k');
-drawLine3d(conv_belt.trigger, 'Color', 'r')
-drawPlane3d(conv_belt.plane, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
+scatter3(cad_projector.detector.detector_points(:,1),...
+    cad_projector.detector.detector_points(:,2),...
+    cad_projector.detector.detector_points(:,3));
+scatter3(cad_projector.source.source_origin_vector(1),...
+    cad_projector.source.source_origin_vector(2),...
+    cad_projector.source.source_origin_vector(3), 100, 'filled', 'k');
+drawLine3d(conveyor_belt.trigger, 'Color', 'r')
+drawPlane3d(conveyor_belt.plane, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
 drawCube(box, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
 axis equal; view(3); rotate3d on
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
-
+hold off
 %% Move mesh to moment of trigger activation
-[translation, ~, closest_pt] = conv_belt.calc_start(moving_mesh);
+[translation, ~, closest_pt] = conveyor_belt.calc_start(moving_mesh);
 moving_mesh.vertices         = moving_mesh.vertices + translation;
 closest_pt = closest_pt + translation;
 
@@ -82,15 +76,15 @@ patch(moving_mesh,'FaceColor', [0.3, 0.8, 0.3], 'FaceAlpha', 0.5, 'EdgeAlpha', 0
 patch(ray, 'FaceColor', 'y', 'FaceAlpha', 0.3, 'EdgeColor', 'None')
 axis equal; view(3); rotate3d on
 hold on
-scatter3(my_cad_projector.detector_points(:,1),...
-    my_cad_projector.detector_points(:,2),...
-    my_cad_projector.detector_points(:,3));
-scatter3(my_cad_projector.source_origin_vector(1),...
-    my_cad_projector.source_origin_vector(2),...
-    my_cad_projector.source_origin_vector(3), 100, 'filled', 'k');
+scatter3(cad_projector.detector.detector_points(:,1),...
+    cad_projector.detector.detector_points(:,2),...
+    cad_projector.detector.detector_points(:,3));
+scatter3(cad_projector.source.source_origin_vector(1),...
+    cad_projector.source.source_origin_vector(2),...
+    cad_projector.source.source_origin_vector(3), 100, 'filled', 'k');
 scatter3(closest_pt(1),closest_pt(2),closest_pt(3),'filled','r')
-drawLine3d(conv_belt.trigger, 'Color', 'r')
-drawPlane3d(conv_belt.plane, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
+drawLine3d(conveyor_belt.trigger, 'Color', 'r')
+drawPlane3d(conveyor_belt.plane, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
 drawCube(box, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
 axis equal; view(3); rotate3d on
 xlabel('X')
@@ -98,20 +92,20 @@ ylabel('Y')
 zlabel('Z')
 
 %% Calculate the position of the mesh for every line scan
-duration    = geometry.n_scans/geometry.line_rate;
+duration    = geometry.n_scans/cad_projector.detector.line_rate;
 start_point = start_point + translation; % start at trigger activation
-end_point   = start_point + conv_belt.speed_vector * duration;
+end_point   = start_point + conveyor_belt.speed_vector * duration;
 positions   = linspaceNDim(start_point, end_point, geometry.n_scans)';
 
 %% Simulate the projection using Lambert-Beer law
 tic
-n_proj      = size(positions, 1);
-projs = NaN(n_proj, geometry.detector_size_px(2));
+n_proj   = size(positions, 1);
+projs    = NaN(n_proj, cad_projector.detector.detector_size_px(2));
 vertices = NaN(size(moving_mesh.vertices,1), 3, n_proj);
 for i = 1:n_proj
 vertices(:,:,i) = mesh.vertices + positions(i,:);
 moving_mesh.vertices = vertices(:,:,i);
-projs(i,:) = my_cad_projector.get_projection(moving_mesh,...
+projs(i,:) = cad_projector.get_projection(moving_mesh,...
                                              'LambertBeer', true,...
                                              'LinearAttenuationCoeff', 0.0015);
 end
@@ -126,14 +120,14 @@ patch(intermediate_mesh,'FaceColor', [0.3, 0.8, 0.3], 'FaceAlpha', 0.5, 'EdgeAlp
 patch(ray, 'FaceColor', 'y', 'FaceAlpha', 0.3, 'EdgeColor', 'None')
 axis equal; view(3); rotate3d on
 hold on
-scatter3(my_cad_projector.detector_points(:,1),...
-    my_cad_projector.detector_points(:,2),...
-    my_cad_projector.detector_points(:,3));
-scatter3(my_cad_projector.source_origin_vector(1),...
-    my_cad_projector.source_origin_vector(2),...
-    my_cad_projector.source_origin_vector(3), 100, 'filled', 'k');
-drawLine3d(conv_belt.trigger, 'Color', 'r')
-drawPlane3d(conv_belt.plane, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
+scatter3(cad_projector.detector.detector_points(:,1),...
+    cad_projector.detector.detector_points(:,2),...
+    cad_projector.detector.detector_points(:,3));
+scatter3(cad_projector.source.source_origin_vector(1),...
+    cad_projector.source.source_origin_vector(2),...
+    cad_projector.source.source_origin_vector(3), 100, 'filled', 'k');
+drawLine3d(conveyor_belt.trigger, 'Color', 'r')
+drawPlane3d(conveyor_belt.plane, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
 drawCube(box, 'FaceColor', [0.9, 0.9, 0.9], 'FaceAlpha', 0.5)
 axis equal; view(3); rotate3d on
 xlabel('X')
