14e412f232051484d0918cae14a4ae1508a689dd
Tim Van De Looverbosch <tim.vandelooverbosch@gmail.com>
2021-05-04 12:03:10 +0200

    Use MEIGO optimization

---
+++ src/predict_geom/meigo_optim.m
@@ -0,0 +1,113 @@
+%% MEIGO Optimization
+clear;
+close all;
+clc;
+%% Define x0
+transf = createRotationOx(deg2rad(25)); % source and detector will be at 25 degree angle from vertical
+% X-ray source
+source_origin_vector = transformPoint3d([0, 0, 470], transf);
+% X-ray detector
+detector_origin_vector = transformPoint3d([0, 0, -150], transf);
+
+% Conveyor belt direction and normal
+trigger_pos = 30;
+placement_tilt = 0;
+euler_mesh = [0 0 0];
+
+x0 = NaN(1,11);
+x0(1:3) = source_origin_vector;
+x0(4:6) = detector_origin_vector;
+x0(7)  = trigger_pos;
+x0(8)  = placement_tilt;
+x0(9:11) = euler_mesh;
+
+%% Boundary conditions
+lb = [-300, -300, 200,... % source
+    -100, -100, -300,...  % detector
+    -110,...              % trigger pos
+    -20,...               % tilt
+    -180, -90, -180];    % euler
+
+ub = [300, 300, 1000,...  % source
+    100, 100, 0.0,...       % detector
+    50,...                 % trigger pos
+    20,...                % tilt
+    180, 90, 180];       % euler
+
+%% Problem specification
+problem.f = 'obj_fun';
+problem.x_L = lb;
+problem.x_U = ub;
+problem.x0 = x0;
+
+opts.maxeval = 10000;
+opts.local.solver = 'fmincon';
+opts.local.iterprint=1;
+
+%% Ground truth image
+flat_field = double(ImportSlices('J:\SET-Mebios_CFD-VIS-DI0327\TimVDL_shared\00_DATA\xray-machine\2021-04-26_phantom_scans\calibration\flat-fields'));
+flat_field = mean(flat_field, 3);
+dark_field = double(ImportSlices('J:\SET-Mebios_CFD-VIS-DI0327\TimVDL_shared\00_DATA\xray-machine\2021-04-26_phantom_scans\calibration\dark-fields'));
+dark_field = mean(dark_field, 3);
+
+input_path = 'J:\SET-Mebios_CFD-VIS-DI0327\TimVDL_shared\00_DATA\xray-machine\2021-04-26_phantom_scans\phantom_plate\phantom_plate.tif';
+img = double(imread(input_path));
+img = flat_field_correction(img, flat_field, dark_field);
+img = imresize(img, 1/10);
+img = img(40:end-41,:);
+scan_gt = img < 0.5;
+figure; imshow(scan_gt)
+
+%% Load phantom mesh
+triangulation = stlread('J:\SET-MEBIOS-POSTHARVEST-DI0414\TimVanDeLooverbosch_u0117721\plate_phantom_robovision.stl');
+mesh.vertices = triangulation.Points;
+mesh.faces = triangulation.ConnectivityList;
+mesh.vertices = (mesh.vertices - mean(mesh.vertices, 1)); % center around origin
+mesh = transformMesh(mesh, createRotationOz(-pi/2));
+mesh = transformMesh(mesh, createRotationOy(pi));
+mesh = ensureManifoldMesh(mesh);
+figure; patch(mesh, 'FaceColor', 'b', 'FaceAlpha', 0.5, 'EdgeColor', 'None')
+axis equal; rotate3d on; view(3)
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
+title('Lego phantom')
+
+%% Compare ground truth and x0
+[scan, line_scanner] = simulate_scan(x0, mesh);
+line_scanner.plot_geometry('Mesh', mesh)
+
+figure;
+subplot(1,2,1)
+imshow(scan_gt)
+subplot(1,2,2)
+imshow(scan)
+
+%% Optimization
+tic;
+Results = MEIGO(problem, opts, 'ESS', mesh, scan_gt, @no_processing, @iou);
+toc
+%% Show result
+[scan, line_scanner] = simulate_scan(Results.xbest, mesh);
+line_scanner.plot_geometry()
+
+figure;
+subplot(1,2,1)
+imshow(scan_gt)
+subplot(1,2,2)
+imshow(scan)
+
+
+%% Functions
+% Process functions
+function dist_img = img2bwdist(img)
+dist_img = 1 - mat2gray(bwdist(~img));
+end
+
+function img = no_processing(img)
+end
+
+% Error functions
+function iou_score = iou(A, B)
+iou_score = 1 - sum(A & B, 'all')/sum(A | B, 'all');
+end
---
+++ src/predict_geom/obj_fun.m
@@ -0,0 +1,17 @@
+function [loss] = obj_fun(x, mesh, scan_gt, process_func, error_func)
+% Simulate scans
+scan_pred = simulate_scan(x, mesh);
+% Process the images
+scan_gt = process_func(scan_gt);
+scan_pred = process_func(scan_pred);
+% Calculate the error
+loss = error_func(scan_gt, scan_pred);
+
+% Equality constrains
+% source = x(1:3);
+% detector = x(4:6);
+% D = distancePoints3d(source, detector);
+% g(1) = D - 625;
+end
+
+
